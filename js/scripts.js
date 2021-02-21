e333956b50db14c6d14a9ca9359161307c8ce04c
Saoud <saoud@saoud.dev>
2021-02-21 12:09:45 -0800

    adding index.html and some logic to scripts.js
    Co-authored-by: Shanen Cross <shanencross@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,52 @@
-//Buisness Logic
+//Business Logic
+function Player(name) {
+    this.name = name;
+    this.score = 0;
+    this.dieValue = 0;
+}
+
+Player.prototype.rollDie = function() {
+
+}
+
+Player.prototype.hold = function() {
+}
+
+function GameManager() {
+    this.currentPlayer;
+    this.isGameRunning = true;
+}
+
+GameManager.prototype.startNewGame = function() {
+    this.isGameRunning = true;
+    let player1 = Player("Player 1");
+    let player2 = Player("Player 2");
+
+    this.currentPlayer = player1;
+    this.runGameLoop();
+}
+
+GameManager.prototype.runGameLoop = function() {
+    while (this.isGameRunning === true) {
+        playerInput = this.checkForInput();
+    }
+}
+
+GameManager.prototype.changeTurn = function() {
+    
+}
+
+GameManager.prototype.checkForVictory = function() {
+    while (this.score < 100) {
+        
+    }
+}
+
+
+
 
 
 //UI Logic
+$(document).ready(function() {
+    
+})
